4b7a80a2b7ebb9de41830cce42e55be8db3e16ea
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2022-10-28 15:46:41 +0000

    'Creating new service ccn2-connectivity-tester'

---
+++ app/uk/gov/hmrc/ccn2connectivitytester/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.ccn2connectivitytester.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
