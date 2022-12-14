b103e00e643feba36a7c8d63e2acd7888e6d1c07
Neil Frow <675806+worthydolt@users.noreply.github.com>
2022-12-14 11:56:40 +0000

    APID-619: correct addressing values for V1 messages

--- app/uk/gov/hmrc/ccn2connectivitytester/models/common/requestsAndResponses.scala
+++ app/uk/gov/hmrc/ccn2connectivitytester/models/common/requestsAndResponses.scala
@@ -44,10 +44,10 @@ class Requests @Inject()(appConfig: AppConfig) {
        |{"wsdlUrl":"${appConfig.wsdlUrlForV1}",
        | "wsdlOperation":"IsAlive", "messageBody":"", "confirmationOfDelivery": true,
        | "addressing": {
-       |  "to":"partner:CCN2.Partner.EU.Customs.TAXUD/ICS_CR.CONF",
-       |  "from":"partner:CCN2.Partner.EU.Customs.TAXUD/ICS_CR.CONF",
-       |  "replyTo":"partner:CCN2.Partner.EU.Customs.TAXUD/ICS_CR.CONF",
-       |  "faultTo":"partner:CCN2.Partner.EU.Customs.TAXUD/ICS_CR.CONF",
+       |  "to":"partner:CCN2.Partner.XI.Customs.TAXUD/ICS_CR.CONF",
+       |  "from":"partner:CCN2.Partner.XI.Customs.TAXUD/ICS_CR.CONF",
+       |  "replyTo":"partner:CCN2.Partner.XI.Customs.TAXUD/ICS_CR.CONF",
+       |  "faultTo":"partner:CCN2.Partner.XI.Customs.TAXUD/ICS_CR.CONF",
        |  "messageId":"$messageId"
        |  },
        |  "confirmationOfDelivery": true,
