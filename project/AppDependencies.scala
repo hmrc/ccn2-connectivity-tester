b3ea3cfeea56a265de98adef2924d7bf0332e500
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2026-01-13 11:15:30 +0000

    API-8891 - Version bunps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,9 +4,9 @@ object AppDependencies {
 
   def apply(): Seq[ModuleID] = compile ++ test
 
-  private val bootstrapVersion    = "9.11.0"
-  private val hmrcMongoVersion    = "2.5.0"
-  private val commonDomainVersion = "0.18.0"
+  private val bootstrapVersion    = "10.5.0"
+  private val hmrcMongoVersion    = "2.11.0"
+  private val commonDomainVersion = "0.19.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"   % bootstrapVersion,
@@ -18,6 +18,6 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion,
-    "org.mockito"             %% "mockito-scala-scalatest"    % "1.17.30"
+    "org.mockito"             %% "mockito-scala-scalatest"    % "2.0.0"
   ).map(_ % "test")
 }
